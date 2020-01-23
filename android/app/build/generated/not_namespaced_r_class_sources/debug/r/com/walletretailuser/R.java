3fb14968822d2166563f873a30bb3735a599d418
anooj1483 <anooj1483@gmail.com>
2020-01-23 11:50:01 +0530

    Verification of Build creation issues

--- android/app/build/generated/not_namespaced_r_class_sources/debug/r/com/walletretailuser/R.java
+++ android/app/build/generated/not_namespaced_r_class_sources/debug/r/com/walletretailuser/R.java
@@ -3791,7 +3791,6 @@ public final class R {
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f0a0000;
-    public static final int ic_launcher_round=0x7f0a0001;
   }
   public static final class raw {
     public static final int app=0x7f0b0000;
